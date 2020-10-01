e0abf688499ca81fcd56eae7fa0bf3b2377188b5
IsaacMcGrind <u16205457@tuks.co.za>
2020-10-01 14:43:35 +0200

    :fire: profile page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { NavComponent } from "./nav/nav.component";
 //import { CanActivateRouteGuard } from "./can-activate-route.guard";
 const routes: Routes = [
   {
